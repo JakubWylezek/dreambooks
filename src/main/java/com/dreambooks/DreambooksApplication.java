9b8333ec616083650190cc908e76acc104a07ac3
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-10-17 09:51:04 +0200

    Initial commit

---
+++ src/main/java/com/dreambooks/DreambooksApplication.java
@@ -0,0 +1,12 @@
+package com.dreambooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreambooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreambooksApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dreambooks/DreambooksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dreambooks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DreambooksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
