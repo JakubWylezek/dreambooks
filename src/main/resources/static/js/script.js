6dfb0db52dfa186767e6dcfc9f15852b4a558658
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-10-17 11:18:05 +0200

    Create index page and add static resources

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,10 @@
+/* =========================================
+                Preloader
+============================================ */
+$(window).on('load', function() {
+  // makes sure that whole site is loaded
+  $('#status').fadeOut();
+  $('#preloader')
+    .delay(350)
+    .fadeOut('slow');
+});
