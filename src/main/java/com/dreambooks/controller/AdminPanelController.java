a7feb046a71b2bb7fce644f10b4d3128e2a7479d
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-10-19 17:48:46 +0200

    Update bookdetails by: -possibillity of removal -possibillity of update

--- src/main/java/com/dreambooks/controller/AdminPanelController.java
+++ src/main/java/com/dreambooks/controller/AdminPanelController.java
@@ -1,8 +1,10 @@
 package com.dreambooks.controller;
 
+import com.dreambooks.model.Book;
 import com.dreambooks.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,5 +37,19 @@ public class AdminPanelController {
         model.addAttribute("book", bookService.getBookById(new Long(id)));
 
         return "/adminpanel/bookdetails";
+    }
+
+    @RequestMapping("/adminpanel/book/save")
+    public String saveBook(@ModelAttribute Book book) {
+        bookService.saveBook(book);
+
+        return "redirect:/adminpanel/books";
+    }
+
+    @RequestMapping("/adminpanel/book/delete/{id}")
+    public String deleteBook(@PathVariable String id) {
+        bookService.deleteBook(new Long(id));
+
+        return "redirect:/adminpanel/books";
     }
 }
