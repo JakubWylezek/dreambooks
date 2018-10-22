e05ff7e140c7ff5628f8ee65a66486b4be65b153
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-10-22 18:45:01 +0200

    Create new main page

---
+++ src/main/java/com/dreambooks/controller/MainPageController.java
@@ -0,0 +1,23 @@
+package com.dreambooks.controller;
+
+import com.dreambooks.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+
+    private BookService bookService;
+
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/main")
+    public String getMainPage(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+
+        return "/mainpage/index";
+    }
+}
