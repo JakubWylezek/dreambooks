d56b7a3b94ce95a639175c4013476ce254456192
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-11-02 16:36:02 +0100

    Change package from jar to war

---
+++ src/main/java/com/dreambooks/Application.java
@@ -0,0 +1,15 @@
+package com.dreambooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+}
