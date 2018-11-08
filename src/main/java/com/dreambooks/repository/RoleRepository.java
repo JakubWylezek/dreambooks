1eedd055cb61d2649181bf5bd93f346293079c88
Jakub Wylężek <jakub.wylezek@wp.pl>
2018-11-08 12:07:52 +0100

    Now administrator can search user by first name or last name

--- src/main/java/com/dreambooks/repository/RoleRepository.java
+++ src/main/java/com/dreambooks/repository/RoleRepository.java
@@ -1,11 +1,11 @@
 package com.dreambooks.repository;
 
 import com.dreambooks.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
+public interface RoleRepository extends CrudRepository<Role, Integer> {
     Role findByRole(String role);
 
 }
--- src/main/java/com/dreambooks/repository/UserRepository.java
+++ src/main/java/com/dreambooks/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.dreambooks.repository;
 
 import com.dreambooks.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
 
     @Query("SELECT COUNT(u) FROM User u")
@@ -18,6 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM User ORDER BY id DESC limit 5", nativeQuery = true)
     Set<User> getMaxFiveNewUsers();
 
-    @Query(value = "SELECT u FROM User u WHERE u.lastName LIKE concat('%',:name,'%')")
+    @Query(value = "SELECT u FROM User u WHERE u.lastName LIKE concat('%',:name,'%') OR u.firstName LIKE concat('%',:name,'%')")
     Set<User> findUsersWithPartOfNames(@Param("name") String name);
 }
